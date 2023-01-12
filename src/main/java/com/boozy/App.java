f7058a873bb0b7f722947eaebf91f6721a90a225
boozy <boozy@noreply.codeberg.org>
2023-01-12 04:04:31 -0300

    Studying JavaFX, since i'll be building upcoming software with this tool.

---
+++ src/main/java/com/boozy/App.java
@@ -0,0 +1,39 @@
+com.boozy;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private Parent createContent() {
+        Rectangle box = new Rectangle(100, 50, Color.BLUE);
+
+        transform(box);
+
+        return new Pane(box);
+    }
+
+    private void transform(Rectangle box) {
+        box.setTranslateX(100);
+        box.setTranslateY(200);
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(createContent(), 800, 600, Color.WHITE));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
